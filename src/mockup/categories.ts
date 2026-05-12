2dff96370eef8f983a69818985e7c448589bc32e
nestnidev <nestnidev17@gmail.com>
2026-05-11 21:53:49 -0500

    feat: add mockup structure for categories and UI components

---
+++ src/mockup/categories.ts
@@ -0,0 +1,38 @@
+export const categories = [
+  {
+    id: 1,
+    name: 'Hardware',
+    icon: '/src/assets/categories/hardware.svg',
+    articles: 20
+  },
+  {
+    id: 2,
+    name: 'Software',
+    icon: '/src/assets/categories/software.svg',
+    articles: 20
+  },
+  {
+    id: 3,
+    name: 'Network',
+    icon: '/src/assets/categories/network.svg',
+    articles: 20
+  },
+  {
+    id: 4,
+    name: 'Performance',
+    icon: '/src/assets/categories/performance.svg',
+    articles: 20
+  },
+  {
+    id: 5,
+    name: 'OS',
+    icon: '/src/assets/categories/os.svg',
+    articles: 20
+  },
+  {
+    id: 6,
+    name: 'Virus',
+    icon: '/src/assets/categories/virus.svg',
+    articles: 20
+  }
+]
