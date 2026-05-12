c4b6919a9ce04b8a7a9f8995058e96c04bace3c8
nestnidev <nestnidev17@gmail.com>
2026-05-11 21:57:36 -0500

    refactor: update paths

--- src/mockup/categories.ts
+++ src/mockup/categories.ts
@@ -2,37 +2,37 @@ export const categories = [
   {
     id: 1,
     name: 'Hardware',
-    icon: '/src/assets/categories/hardware.svg',
+    icon: '/categories/hardware.svg',
     articles: 20
   },
   {
     id: 2,
     name: 'Software',
-    icon: '/src/assets/categories/software.svg',
+    icon: '/categories/software.svg',
     articles: 20
   },
   {
     id: 3,
     name: 'Network',
-    icon: '/src/assets/categories/network.svg',
+    icon: '/categories/network.svg',
     articles: 20
   },
   {
     id: 4,
     name: 'Performance',
-    icon: '/src/assets/categories/performance.svg',
+    icon: '/categories/performance.svg',
     articles: 20
   },
   {
     id: 5,
     name: 'OS',
-    icon: '/src/assets/categories/os.svg',
+    icon: '/categories/os.svg',
     articles: 20
   },
   {
     id: 6,
     name: 'Virus',
-    icon: '/src/assets/categories/virus.svg',
+    icon: '/categories/virus.svg',
     articles: 20
   }
 ]
