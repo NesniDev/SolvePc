0570843622b19f7b6e9221d5b433f524abf48acd
nestnidev <nestnidev17@gmail.com>
2026-05-17 18:01:44 -0500

    feat: fetch data from API endpoints

---
+++ src/api/get/getSolutions.ts
@@ -0,0 +1,20 @@
+
+
+import type { Solutions } from "src/interfaces/Solutions";
+import { getSolutions } from "../solutions";
+
+
+
+export const getAllSolutions = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 2000)) // simulación
+
+  const response = await getSolutions.get< Solutions[]>("/solutions");
+  
+  if (response.status !== 200) {
+    throw new Error("Error en la API")
+  }
+
+  return response.data;
+
+
+}
